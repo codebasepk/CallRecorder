7385bb0e4b8665fdc034dddb20378ea96fabe276
husnain446 <husnain446@gmail.com>
2015-09-01 09:35:11 +0500

    add stag tabs

--- app/src/main/java/com/byteshatf/callrecorder/MainActivity.java
+++ app/src/main/java/com/byteshatf/callrecorder/MainActivity.java
@@ -1,7 +1,18 @@
 package com.byteshatf.callrecorder;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.*;
+import android.support.v4.app.ListFragment;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.PhoneStateListener;
@@ -9,14 +20,26 @@ import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 import com.byteshatf.callrecorder.contactpicker.ContactsPicker;
 
-public class MainActivity extends AppCompatActivity {
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
+
+public class MainActivity extends AppCompatActivity implements MaterialTabListener {
+
     private Helpers mHelpers;
     private IncomingCallListener mIncomingCallListener;
     private TelephonyManager mTelephonyManager;
+    private String[] mListTitles;
+    private ViewPager mViewPager;
+    private MaterialTabHost mMaterialTabHost;
+    private Resources mResources;
+    private Fragment mFragment;
 
 
 
@@ -24,12 +47,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#689F39")));
+        getSupportActionBar().setElevation(0);
         mHelpers = new Helpers(getApplicationContext());
         mIncomingCallListener = new IncomingCallListener();
         mTelephonyManager = mHelpers.getTelephonyManager();
         mTelephonyManager.listen(mIncomingCallListener, PhoneStateListener.LISTEN_CALL_STATE);
         IntentFilter intentFilter = new IntentFilter(Intent.ACTION_NEW_OUTGOING_CALL);
         registerReceiver(mIncomingCallListener.mOutgoingCall, intentFilter);
+        showDialogForFirstTime();
+
+        mMaterialTabHost = (MaterialTabHost) findViewById(R.id.tab_host);
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mResources = getResources();
+        ViewPagerAdapter mViewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mViewPagerAdapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                mMaterialTabHost.setSelectedNavigationItem(position);
+            }
+        });
+        for (int i = 0; i < mViewPagerAdapter.getCount(); i++) {
+            mMaterialTabHost.addTab(mMaterialTabHost.newTab().setIcon(getIcon(i)).setTabListener(this));
+        }
     }
 
     @Override
@@ -50,8 +91,86 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             Intent intent = new Intent(getApplicationContext(), AddNewContactDetailActivity.class);
             startActivity(intent);
-        }
+          }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showDialogForFirstTime() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Welcome!");
+        builder.setMessage("Would you like to add your recording rule?");
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(getApplicationContext(),AddNewContactDetailActivity.class);
+                startActivity(intent);
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onTabSelected(MaterialTab materialTab) {
+
+    }
+
+    @Override
+    public void onTabReselected(MaterialTab materialTab) {
+        mViewPager.setCurrentItem(materialTab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(MaterialTab materialTab) {
+
+    }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public Fragment getItem(int num) {
+            mFragment = new Fragment();
+
+            switch (num) {
+                case 0:
+                    mFragment = new RecordingListFragment();
+                    break;
+                case 1:
+                    mFragment = new RulesFragment();
+                    break;
+            }
+            return mFragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return null;
+        }
+    }
+
+    private Drawable getIcon(int position) {
+        switch (position) {
+            case 0:
+                return mResources.getDrawable(R.drawable.list);
+            case 1:
+                return mResources.getDrawable(R.drawable.rule);
+            default:
+                return null;
+        }
     }
 }
