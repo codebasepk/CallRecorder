b835d63855bd1855b05be964ddf317813e8554d9
s9iper1 <s9iper1@gmail.com>
2015-09-02 23:21:41 +0500

    add update

--- app/src/main/java/com/byteshatf/callrecorder/database/DatabaseConstants.java
+++ app/src/main/java/com/byteshatf/callrecorder/database/DatabaseConstants.java
@@ -7,7 +7,6 @@ public class DatabaseConstants {
     public static final int DATABASE_VERSION = 1;
     public static final String TABLE_NAME = "call_recording";
     public static final String TITLE = "title";
-    public static final String STATE = "state";
     public static final String CONTACTS = "contacts";
     public static final String ID_COLUMN = "ID";
     public static final String DATE_COLUMN = "DATETIME";
@@ -17,7 +16,6 @@ public class DatabaseConstants {
                     TABLE_NAME + "(" +
                     ID_COLUMN + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     TITLE + " TEXT UNIQUE , " +
-                    STATE + " TEXT , " +
                     CONTACTS + " TEXT, " +
                     DATE_COLUMN + " TEXT" + " ) ";
 }
