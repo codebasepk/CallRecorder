314d7332346f3a77f23d133c31fae2dec20031a9
husnain446 <husnain446@gmail.com>
2015-09-02 14:25:25 +0500

    main spinner values now save

--- app/src/main/java/com/byteshatf/callrecorder/fragments/RulesFragment.java
+++ app/src/main/java/com/byteshatf/callrecorder/fragments/RulesFragment.java
@@ -7,21 +7,41 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.byteshatf.callrecorder.AddRuleActivity;
+import com.byteshatf.callrecorder.AppGlobals;
+import com.byteshatf.callrecorder.Helpers;
 import com.byteshatf.callrecorder.R;
 
-public class RulesFragment extends android.support.v4.app.Fragment implements View.OnClickListener {
+public class RulesFragment extends android.support.v4.app.Fragment implements View.OnClickListener,
+        Spinner.OnItemSelectedListener {
 
     private View baseView;
     private Button mButton;
+    private Spinner mSpinner;
+    private Helpers mHelpers;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         baseView = inflater.inflate(R.layout.rules_fragment, container, false);
+        mHelpers = new Helpers(getActivity());
         mButton = (Button) baseView.findViewById(R.id.button);
+        mSpinner = (Spinner) baseView.findViewById(R.id.main_spinner);
+        mSpinner.setOnItemSelectedListener(this);
+        String[] mainSpinner = {"All Incoming Calls", "All Outgoing Calls", "All Incoming/outgoing",
+                "Selected Only"};
+        ArrayAdapter<String> arrayAdapter;
+        arrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item,
+                mainSpinner);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(arrayAdapter);
+        mSpinner.setSelection(mHelpers.getSpinnerValue("key"));
+        System.out.println(mHelpers.getSpinnerValue("key"));
         mButton.setOnClickListener(this);
         return baseView;
     }
@@ -33,6 +53,17 @@ public class RulesFragment extends android.support.v4.app.Fragment implements Vi
                 startActivity(new Intent(getActivity(), AddRuleActivity.class));
                 break;
         }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        mHelpers.saveSpinnerState("key" ,parent.getSelectedItemPosition());
+        System.out.println(parent.getSelectedItemPosition());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
