57fd11caef616f0d6a5785cc95f9ead315c6f864
s9iper1 <s9iper1@gmail.com>
2015-09-02 12:36:51 +0500

    add database and handle multiple conditions

---
+++ app/src/main/java/com/byteshatf/callrecorder/database/DatabaseHelpers.java
@@ -0,0 +1,42 @@
+package com.byteshatf.callrecorder.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.byteshatf.callrecorder.AppGlobals;
+import com.byteshatf.callrecorder.Helpers;
+
+
+public class DatabaseHelpers extends SQLiteOpenHelper {
+
+    public DatabaseHelpers(Context context) {
+        super(context, DatabaseConstants.DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DatabaseConstants.TABLE_CREATE);
+        Log.i(AppGlobals.getLogTag(getClass()), "Database created !!!");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS" + DatabaseConstants.TABLE_NAME);
+        onCreate(db);
+    }
+
+    public void createNewEntry(String title, int state, String contacts) {
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DatabaseConstants.TITLE, title);
+        values.put(DatabaseConstants.STATE, state);
+        values.put(DatabaseConstants.CONTACTS, contacts);
+        values.put(DatabaseConstants.DATE_COLUMN, Helpers.getTimeStampForDatabase());
+        sqLiteDatabase.insert(DatabaseConstants.TABLE_NAME, null, values);
+        Log.i(AppGlobals.getLogTag(getClass()), "created New Entry");
+        sqLiteDatabase.close();
+    }
+}
