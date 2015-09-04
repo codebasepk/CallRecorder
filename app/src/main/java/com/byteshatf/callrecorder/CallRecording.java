6a2a1631bfe676d174efb411253c01dd02661186
s9iper1 <s9iper1@gmail.com>
2015-09-04 15:41:11 +0500

    revert

--- app/src/main/java/com/byteshatf/callrecorder/CallRecording.java
+++ app/src/main/java/com/byteshatf/callrecorder/CallRecording.java
@@ -47,8 +47,6 @@ public class CallRecording {
             isRecording = true;
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (RuntimeException e) {
-            Log.i(AppGlobals.getLogTag(getClass()), "crash at audio Source");
         }
     }
 
