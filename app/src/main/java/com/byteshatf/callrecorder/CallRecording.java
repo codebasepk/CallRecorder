d635d4d62eb9eb4327516d8989064019e7e9ef6b
scorpion1 <shgr8rb@gmail.com>
2015-09-09 17:28:32 +0500

    set actionbar icons

--- app/src/main/java/com/byteshatf/callrecorder/CallRecording.java
+++ app/src/main/java/com/byteshatf/callrecorder/CallRecording.java
@@ -5,6 +5,8 @@ import android.media.MediaRecorder;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 public class CallRecording {
 
     public static boolean isRecording;
@@ -15,34 +17,21 @@ public class CallRecording {
     public void startRecord() {
         mHelpers = new Helpers(AppGlobals.getContext());
         sharedPreferences = mHelpers.getPreferenceManager();
-        int recordingSource = sharedPreferences.getInt("radio_int", 0);
         mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setAudioEncodingBitRate(96000);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mediaRecorder.setOutputFile(AppGlobals.getDataDirectory("CallRec") + "/"
+                + AppGlobals.sCurrentNumber + "_" + Helpers.getTimeStamp() + ".aac");
         try {
-            switch (recordingSource) {
-                case 0:
-                    mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                    break;
-                case 1:
-                    mediaRecorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
-                    break;
-                case 2:
-                    mediaRecorder.setAudioSource(MediaRecorder.AudioSource.VOICE_DOWNLINK);
-                    break;
-                case 3:
-                    mediaRecorder.setAudioSource(MediaRecorder.AudioSource.VOICE_UPLINK);
-                    break;
-            }
-            mediaRecorder.setAudioEncodingBitRate(96000);
-            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
-            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-            mediaRecorder.setOutputFile(AppGlobals.getDataDirectory("CallRec") + "/" + AppGlobals.sCurrentNumber + "_" + Helpers.getTimeStamp() + ".aac");
             mediaRecorder.prepare();
-            mediaRecorder.start();
-            Log.i(AppGlobals.getLogTag(getClass()), "Recording started.....");
-            isRecording = true;
-        } catch (Exception e) {
-            Toast.makeText(AppGlobals.getContext(), "Error: Recording source incompatible", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        mediaRecorder.start();
+        Log.i(AppGlobals.getLogTag(getClass()), "Recording started.....");
+        isRecording = true;
     }
 
     public void stopRecording() {
